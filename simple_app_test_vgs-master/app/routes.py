14c7958f821947134deeea1a38ca88a1d9ba3164
gshaish01 <66346471+gshaish01@users.noreply.github.com>
2020-10-19 08:55:38 -0700

    Add files via upload

---
+++ simple_app_test_vgs-master/app/routes.py
@@ -0,0 +1,44 @@
+from app import app
+from flask import render_template, request, jsonify
+import requests
+import os
+import json
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+
+
+@app.route("/forward", methods=['POST'])
+def forward():
+    cc_cvv = request.form['cc_cvv']
+    cc_number = request.form['cc_number']
+    cc_exp = request.form['cc_exp']
+
+
+    response_i = requests.post("https://tntsvizavbe.sandbox.verygoodproxy.com//post",
+                             json={'cvv': cc_cvv, 'card_number': cc_number, 'exp_date': cc_exp})
+    #print(str(response_i.text))
+    response_i=response_i.json()
+    J_card_number=(json.dumps(response_i['json']['card_number'], sort_keys=True, indent=4).strip('"'))
+    J_cvv=(json.dumps(response_i['json']['cvv'], sort_keys=True, indent=4).strip('"'))
+    J_exp_date=(json.dumps(response_i['json']['exp_date'], sort_keys=True, indent=4).strip('"'))
+
+
+
+
+    os.environ[
+        'HTTPS_PROXY'] = 'http://UShxsFJgSucAVYDhj4epQ1kF:2e9e0f97-45e4-4095-adce-bc523cd599ac@tntsvizavbe.sandbox.verygoodproxy.com:8080'
+    response = requests.post('https://echo.apps.verygood.systems/post',
+                             json={'cvv': J_cvv, 'card_number': J_card_number, 'exp_date': J_exp_date },
+                             verify='sandbox.pem')
+
+    #print(str(response.text))
+    response = response.json()
+
+
+
+    return render_template('forward.html', response=response,response_i=response_i)
